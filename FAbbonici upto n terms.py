59730b5f3f16e14fb4c2f38fce58526926147fae
roshmeet-17 <75925727+roshmeet-17@users.noreply.github.com>
2020-12-13 12:21:17 +0530

    Add files via upload

---
+++ FAbbonici upto n terms.py
@@ -0,0 +1,39 @@
+print("Fabbonici Series")
+
+
+num1=int(input("Enter the limit upto you want to print Fabbonici series:- "))
+first=0
+second =1
+print(first)
+print (second)
+for i in range (1,num1+1):
+    third = first + second
+    print (third)
+    first,second=second,third
+
+
+
+print("Fabbonici Series upto 10 :--->")
+first=0
+second=1
+print(first)
+print (second)
+for i in range(1,6):
+    third = first + second
+    print (third)
+    first,second=second,third
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
